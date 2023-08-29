8a6b0cdc8c4080383ba466a6bad9f07639a247d2
Anvar717 <anvarshokirov11@gmail.com>
2023-08-29 10:39:17 +0500

    dfg

--- src/views/contractinformation/application/view.vue
+++ src/views/contractinformation/application/view.vue
@@ -13,7 +13,7 @@
                             </b-button>
                         </b-col>
                     </b-row> -->
-                    <iframe src="https://talaba.e-edu.uz/api/public/download/TEMPLATE-Ici5y692164604.txt" width="1333px" height="700px">
+                    <!-- <iframe src="https://talaba.e-edu.uz/api/public/download/TEMPLATE-Ici5y692164604.txt" width="1333px" height="700px"> -->
                     </iframe>
                 </b-card>
             </b-col>
