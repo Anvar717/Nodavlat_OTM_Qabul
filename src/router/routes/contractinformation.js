0fc389ad6865c9640bd2d0254c6670a83ec9435f
Anvar717 <anvarshokirov11@gmail.com>
2023-09-01 12:22:55 +0500

    sdfdsfs

--- src/router/routes/contractinformation.js
+++ src/router/routes/contractinformation.js
@@ -51,40 +51,6 @@ export default [
     },
   },
   {
-    path: '/contractinformation/application',
-    name: 'application',
-    component: () => import('@/views/contractinformation/application/index.vue'),
-    meta: {
-      pageTitle: 'Application',
-      breadcrumb: [
-        {
-          text: 'Contractinformation',
-        },
-        {
-          text: 'Application',
-          active: true,
-        },
-      ]
-    },
-  },
-  {
-    path: '/contractinformation/application/view/id=:id',
-    name: 'ViewApplication',
-    component: () => import('@/views/contractinformation/application/view.vue'),
-    meta: {
-      pageTitle: 'Appliaction',
-      breadcrumb: [
-        {
-          text: 'Contractinformation',
-        },
-        {
-          text: 'Appliaction',
-          active: true,
-        },
-      ]
-    },
-  },
-  {
     path: '/contractinformation/template',
     name: 'template',
     component: () => import('@/views/contractinformation/template/index.vue'),
