bcef4106c5e46d999ac273d3dab2701599972a3b
Anvar717 <anvarshokirov11@gmail.com>
2023-09-06 19:35:04 +0500

    sdfdsfs

--- src/navigation/vertical/managment.js
+++ src/navigation/vertical/managment.js
@@ -1,32 +1,36 @@
 export default [
-    {
-      header: 'Management',
-    },
-    {
-      title: 'Management',
-      icon: 'SettingsIcon',
-      children : [
-          // {
-          //   title : 'Organization',
-          //   route: 'organization',
-          //   visible : 'OrganizationView'
-          // },
-          // {
-          //   title : 'OrganizationType',
-          //   route: 'organizationtype',
-          //   visible : "OrganizationView"
-          // },
-          // {
-          //   title : 'User',
-          //   route: 'user',
-          //   visible : 'UserView'
-          // },
-          // {
-          //   title : 'Role',
-          //   route: 'role',
-          //   visible : 'RoleView'
-          // },
-      ]
-    }
-  ]
-  
+  {
+    header: "Management",
+  },
+  {
+    title: "Management",
+    icon: "SettingsIcon",
+    children: [
+      {
+        title: "OtmUser",
+        route: "otmuser",
+        visible: true,
+      },
+      // {
+      //   title : 'Organization',
+      //   route: 'organization',
+      //   visible : 'OrganizationView'
+      // },
+      // {
+      //   title : 'OrganizationType',
+      //   route: 'organizationtype',
+      //   visible : "OrganizationView"
+      // },
+      // {
+      //   title : 'User',
+      //   route: 'user',
+      //   visible : 'UserView'
+      // },
+      // {
+      //   title : 'Role',
+      //   route: 'role',
+      //   visible : 'RoleView'
+      // },
+    ],
+  },
+];
